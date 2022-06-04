d9079adecf666d818c0a13e445a1b47e97f09bc4
dawole94 <dawid.oleksy94@gmail.com>
2022-06-04 16:36:11 +0200

    change

--- ideabank.py
+++ ideabank.py
@@ -2,4 +2,12 @@ import sys
 
 
 # print(sys.argv)
-print(sys.argv[1])
+list = ["pomysł 1", "pomysł 2", "pomysł 3"]
+
+
+if len(sys.argv) > 1:
+    param1 = sys.argv[1]
+    print(sys.argv[1])
+    if param1 == "--list":
+        for i in list:
+            print(i)
